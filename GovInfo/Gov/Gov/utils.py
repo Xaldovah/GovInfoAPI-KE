489d02bd1266d4df37dc3c37ead013fff0bfb5e2
xaldovah <dxaldovah@gmail.com>
2024-03-25 02:45:42 +0300

    Added chromdriver to automatically click on the page to extract mps data

--- GovInfo/Gov/Gov/utils.py
+++ GovInfo/Gov/Gov/utils.py
@@ -1,29 +1,58 @@
-import sys
-sys.path.append('..')
-import requests
-from bs4 import BeautifulSoup
-from parliament.models import MemberOfParliament
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 import time
+from parliament.models import MemberOfParliament
 
-def find_mps():
-    url = 'http://www.parliament.go.ke/the-national-assembly/mps'
-    response = requests.get(url).text
+def extract_page_data(driver):
+    """
+    Function to extract the data from the current page of the pagination element. 
+    Returns a list of dictionaries, each containing the data from one row of the table.
+    """
+    page_output = []
 
-    soup = BeautifulSoup(response, 'lxml')
-    mp_elems = soup.find_all('tr', class_='mp')
+    page_elements = driver.find_elements(By.XPATH, '//tr[contains(@class, "mp")]')
+    for each_element in page_elements:
+        name = each_element.find_element(By.CLASS_NAME, 'views-field-field-name').text.strip()
+        constituency = each_element.find_element(By.CLASS_NAME, 'views-field-field-constituency').text.strip()
+        county = each_element.find_element(By.CLASS_NAME, 'views-field-field-county').text.strip()
+        party = each_element.find_element(By.CLASS_NAME, 'views-field-field-party').text.strip()
 
-    for index, mp in enumerate(mp_elems):
-        name = mp.find('td', class_='views-field views-field-field-name').text.strip()
-        constituency = mp.find('td', class_='views-field views-field-field-constituency').text.strip()
-        county = mp.find('td', class_='views-field views-field-field-county').text.strip()
-        party = mp.find('td', class_='views-field views-field-field-party').text.strip()
-        
-        if constituency:
-            mp_object = MemberOfParliament(name=name, constituency=constituency, county=county, party=party)
+        mp_data = {
+                'name': name,
+                'constituency': constituency,
+                'county': county,
+                'party': party
+        }
+
+        page_output.append(mp_data)
+    return page_output
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) 
+driver.get('http://www.parliament.go.ke/the-national-assembly/mps')
+driver.maximize_window()
+
+# Loop through each page
+while True:
+    # Extract data from the current page
+    page_data = extract_page_data(driver)
+
+    # Save data to the SQLite database
+    for mp_data in page_data:
+        existing_mp = MemberOfParliament.objects.filter(name=mp_data['name'], constituency=mp_data['constituency'], county=mp_data['county'], party=mp_data['party']).first()
+        if not existing_mp:
+            mp_object = MemberOfParliament(**mp_data)
             mp_object.save()
 
+    # Check if there's a next page
+    try:
+        next_page = driver.find_element(By.XPATH, '//a[contains(@title, "Go to next page")]')   
+        next_page.click()
+        time.sleep(2)
+    except NoSuchElementException:
+        print("Reached the end of pagination.")
+        break
 
-if __name__ == '__main__':
-    while True:
-        find_mps()
-        time.sleep(24 * 60 * 60)
+# Close the browser
+driver.quit()
--- GovInfo/Gov/Gov/views.py
+++ GovInfo/Gov/Gov/views.py
@@ -1,3 +1,4 @@
+import asyncio
 from django.http import JsonResponse
 from .models import Gov
 from parliament.models import MemberOfParliament
@@ -27,7 +28,7 @@ def gov_list(request, format=None):
     Gets elective offices
     """
     if request.method == 'GET':
-        find_mps()
+        extract_page_data(driver)
         gov = Gov.objects.all()
         serializer = GovSerializer(gov, many=True)
         return Response(serializer.data)
@@ -45,16 +46,23 @@ def mps_list(request, format=None):
     Gets all the mps
     """
     if request.method == 'GET':
+        extract_page_data(driver)
         mps = MemberOfParliament.objects.all()
         serializer = MpSerializer(mps, many=True)
         return Response(serializer.data)
 
     if request.method == 'POST':
-        serializer = MpSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+        data = request.data
+        existing_mps = MemberOfParliament.objects.all()
+        
+        for mp_data in data:
+            existing_mp = existing_mps.filter(name=mp_data['name'], constituency=mp_data['constituency'], county=mp_data['county'], party=mp_data['party']).first()
+            if existing_mp:
+                continue
+            serializer = MpSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def mps_detail(request, id, format=None):
@@ -62,6 +70,7 @@ def mps_detail(request, id, format=None):
     Gets, updates and deletes an mp
     """
     try:
+        extract_page_data(driver)
         mp = MemberOfParliament.objects.get(pk=id)
     except MemberOfParliament.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
